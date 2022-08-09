4c4b11e06e28a88655fab6946e52e38047f992e7
carbaaa <saco030402@gmail.com>
2022-08-09 11:25:03 -0300

    second commit

--- pong.py
+++ pong.py
@@ -6,6 +6,11 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+
+score_a = 0
+score_b = 0
+
 #Paddle A
 paddle_a= turtle.Turtle()
 paddle_a.speed(0)
@@ -34,7 +39,18 @@ ball.goto(0, 0)
 ball.dx = 0.1
 ball.dy = -0.1
 
-#Function
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Player A: 0 Player B: 0', align='center', font=('Courier', 24, 'normal'))
+
+
+
+# Function
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -82,11 +98,21 @@ while True:
     if ball.xcor() >390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a +=1
+        pen.clear()
+        pen.write(f"Player A: {score_a} Player B: {score_b}", align='center', font=('Courier', 24, 'normal'))
+
     if ball.xcor() <-390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b =+1
+        pen.clear()
+        pen.write(f"Player A: {score_a} Player B: {score_b}", align='center', font=('Courier', 24, 'normal'))
 
     #Collisions
     if (ball.xcor() > 340 and ball.xcor()< 350) and (ball.ycor() < paddle_b.ycor() +40 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
+        ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor()> -350) and (ball.ycor() < paddle_a.ycor() +40 and ball.ycor() > paddle_a.ycor() -40):
+        ball.setx(-340)
         ball.dx *= -1
